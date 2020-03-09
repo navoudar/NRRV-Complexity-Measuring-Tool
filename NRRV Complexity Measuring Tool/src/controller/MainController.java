e3a5099da8750a049fddbd8f356fcb0583416014
navoudar <navodudara18@gmail.com>
2020-03-09 18:23:33 +0530

    file reading done

--- NRRV Complexity Measuring Tool/src/controller/MainController.java
+++ NRRV Complexity Measuring Tool/src/controller/MainController.java
@@ -1,13 +1,16 @@
 package controller;
 import java.util.Arrays;
+
+import navod.KeywordAndReservedWord;
 public class MainController {
 
 	String Content[];
+	KeywordAndReservedWord kwrw = new KeywordAndReservedWord();
 	public void getFileContent(String content[]) {
 		System.out.println("In main conroller");
 		Content = content;
-		System.out.println(Arrays.toString(Content));
-		
+	//	System.out.println(Arrays.toString(Content));
+		kwrw.Complexity(Content);
 	}
 	
 }
